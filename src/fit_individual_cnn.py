0fdaf4dc64813eda5c3af61b96d28f34a89f46e5
laurabpaulsen <202005791@post.au.dk>
2024-05-09 11:44:23 +0200

    simplify script by moving some code to utils.cnn

--- src/fit_individual_cnn.py
+++ src/fit_individual_cnn.py
@@ -12,57 +12,38 @@ from sklearn.model_selection import GridSearchCV
 import sys
 sys.path.append(str(Path(__file__).parents[1]))
 
-from utils.cnn import Net
+from utils.cnn import get_cnn_model, return_param_grid
 
 
 def main():
     path = Path(__file__)
 
-    ### SETTING UP PARAMETERS ###
-    max_epochs = 3 ## UP THIS ALOT  
-    # prep for grid search
-    param_grid = {
-        "lr": [0.0001, 0.001, 0.01],
-        "batch_size": [4, 8],
-
-    }
-
-    train_subjects = ["sub-01", "sub-02"]
+    param_grid = return_param_grid()
+    train_subjects = ["sub-01", "sub-02", "sub-03", "sub-04", "sub-05", "sub-06", "sub-07", "sub-08"]
 
     for subj in train_subjects:
 
         gaf_path = path.parents[1] / "data" / "gaf" 
-        gafs, labels = np.load(gaf_path / subj / f"{subj}_gafs_train.npy"), np.load(gaf_path / subj / f"{subj}_labels_train.npy")
+        gafs, labels = np.load(gaf_path / subj / f"gafs_train.npy"), np.load(gaf_path / subj / f"labels_train.npy")
 
         # change labels to LongTensor to avoid the error: RuntimeError: Expected object of scalar type Long but got scalar type Float
         labels = torch.LongTensor(labels) 
 
-        net = NeuralNetClassifier(
-            Net,
-            max_epochs = max_epochs,
-            criterion = nn.CrossEntropyLoss,
-            optimizer = optim.SGD,
-            iterator_train__shuffle = True
-        )
+        model = get_cnn_model()
 
         # run grid search
-        gs = GridSearchCV(net, param_grid, cv=10, scoring='accuracy', verbose=2)
+        gs = GridSearchCV(model, param_grid, cv=5, scoring='accuracy', verbose=1)
         gs.fit(gafs, labels)
 
         # generate the model with the best parameters to save
         best_params = gs.best_params_
 
-        model = NeuralNetClassifier(
-            Net,
-            max_epochs = max_epochs,
-            criterion = nn.CrossEntropyLoss,
-            optimizer = optim.SGD,
-            iterator_train__shuffle = True,
+        model2 = get_cnn_model(
             lr = best_params['lr'],
             batch_size = best_params['batch_size']
         )
 
-        model.fit(gafs, labels)
+        model2.fit(gafs, labels)
 
         # create output dir for the subject
         output_dir = path.parents[1] / "mdl" / subj
@@ -70,12 +51,7 @@ def main():
         if not output_dir.exists():
             output_dir.mkdir(parents=True)
 
-        torch.save(model, output_dir / f"model.pt")
-
-
-
-    
-
+        torch.save(model2, output_dir / f"model.pt")
 
 if __name__ == "__main__":
     main()
